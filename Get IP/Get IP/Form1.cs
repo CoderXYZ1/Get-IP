a108edbe1c361fd06e3cb494be7942cdfe184ee7
CoderXYZ1 <43748934+CoderXYZ1@users.noreply.github.com>
2018-10-01 05:27:48 -0700

    Source ;)

---
+++ Get IP/Get IP/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+
+namespace Mac_Changer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (WebClient client = new WebClient())
+            {
+                string htmlCode = client.DownloadString("https://www.myexternalip.com/raw");
+                textBox1.Text = htmlCode;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
